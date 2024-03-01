e3815305c38e4c7aaddc0103edacf1ab91951801
Max Althoff <11089186+maxalthoff@users.noreply.github.com>
2024-03-01 09:53:50 -0600

    Add Chapter 01 Exercise 09

---
+++ src/chapter01/Ex_01_09.java
@@ -0,0 +1,11 @@
+package chapter01;
+
+public class Ex_01_09 {
+    public static void main(String[] args) {
+        System.out.println("The area of a 4.5 by 7.9 rectangle is");
+        System.out.println(4.5 * 7.9);
+        System.out.println();
+        System.out.println("The perimeter of a 4.5 by 7.9 rectangle is");
+        System.out.println(2 * 4.5 + 2 * 7.9);
+    }
+}
