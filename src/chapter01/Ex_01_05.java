64d19dd29a109d5af1f2c6062d713f792d9c09c6
Max Althoff <11089186+maxalthoff@users.noreply.github.com>
2024-03-01 09:42:50 -0600

    Add Chapter 01 Exercise 05

---
+++ src/chapter01/Ex_01_05.java
@@ -0,0 +1,8 @@
+package chapter01;
+
+public class Ex_01_05 {
+    public static void main(String[] args) {
+        System.out.println("(9.5 x 4.5 - 2.5 x 3) / (45.5 - 3.5) is ");
+        System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }
+}
