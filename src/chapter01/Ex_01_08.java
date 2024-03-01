f499d57b6bc15bcd8a953173a1965bc21d77dd79
Max Althoff <11089186+maxalthoff@users.noreply.github.com>
2024-03-01 09:52:04 -0600

    Add Chapter 01 Exercise 08

---
+++ src/chapter01/Ex_01_08.java
@@ -0,0 +1,11 @@
+package chapter01;
+
+public class Ex_01_08 {
+    public static void main(String[] args) {
+        System.out.println("The area of a circle with radius 5.5 is");
+        System.out.println(5.5 * 5.5 * 3.14159);
+        System.out.println();
+        System.out.println("The perimeter of a circle with radius 5.5 is");
+        System.out.println(2 * 5.5 * 3.14159);
+    }
+}
