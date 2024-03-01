b0e8c2a8f082d985eecc91d69c0831faf9866680
Max Althoff <11089186+maxalthoff@users.noreply.github.com>
2024-03-01 09:48:27 -0600

    Add Chapter 01 Exercise 07

---
+++ src/chapter01/Ex_01_07.java
@@ -0,0 +1,11 @@
+package chapter01;
+
+public class Ex_01_07 {
+    public static void main(String[] args) {
+        System.out.println("4 x (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11) is ");
+        System.out.println(4.0 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11));
+        System.out.println();
+        System.out.println("4 x (1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13) is ");
+        System.out.println(4.0 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13));
+    }
+}
